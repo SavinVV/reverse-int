67ed77f46af92a07653967cad0673aab6fcfdc62
Vasiliy Savin <v.savin94@mail.ru>
2021-03-05 17:00:31 +0300

    fix: create functions reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
-}
+    let number = '';
+    n = Math.abs(n);
+    for (let i = String(n).length - 1; i >= 0; i--) {
+        number += String(n)[i];
+    }
+    return number;
+};
